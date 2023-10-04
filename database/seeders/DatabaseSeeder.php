84ecf3e3a0c382f04194367d16dfd2339f831c02
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-10-04 23:38:43 +0700

    changed user db (email -> username)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'username' => 'root',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
