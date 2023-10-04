640a745d59e947d9008aff88f7117fed366cb9ad
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-10-04 23:37:58 +0700

    added basic login for root

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class LoginController extends Controller
+{
+    public function auth(Request $request)
+    {
+        $auth = $request->username;
+        $password = $request->password;
+        $checkUser = User::where('username', $auth)->first();
+   
+            if($checkUser != null){
+           
+                    $credentials = ([
+                        'username' => $auth,
+                        'password' => $password,
+                    ]);
+
+                    
+                    if (Auth::attempt($credentials)) {
+                            return redirect()->route('home');
+                    }
+        
+            }elseif ($checkUser == null ){
+                Alert::error('Failed', 'Gagal login');
+                return redirect()->back();
+            }
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        
+        Auth::logout();
+
+        return redirect()->route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
+})->name('login');
+
+Route::post('auth', [LoginController::class, 'auth'])->name('auth');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('home');
 });
