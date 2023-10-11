10f6bf592c09ba7466326c78ea3760e72617a98c
aesencodes <adjiesuryanugraha1@gmail.com>
2023-10-12 01:32:27 +0700

    fixed landing page

--- resources/views/pages/home/landing.blade.php
+++ resources/views/pages/home/landing.blade.php
@@ -71,11 +71,9 @@
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-sm-10 col-lg-8">
-                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Best Online Courses</h5>
-                                <h1 class="display-3 text-white animated slideInDown">The Best Online Learning Platform</h1>
-                                <p class="fs-5 text-white mb-4 pb-2">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea sanctus eirmod elitr.</p>
-                                <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
-                                <a href="" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
+                                <h5 class="text-primary text-uppercase mb-3 animated slideInDown">Platform Pembelajaran Online</h5>
+                                <h1 class="display-3 text-white animated slideInDown">Pengalaman pembelajaran interaktif</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">Memfasilitasi pengajar dengan alat pembelajaran yang inovatif. Menciptakan lingkungan pembelajaran yang aman dan terarah.</p>
                             </div>
                         </div>
                     </div>
@@ -94,8 +92,8 @@
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
                             <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
-                            <h5 class="mb-3">Skilled Instructors</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <h5 class="mb-3">Manajemen Tugas</h5>
+                            <p>Mengelola tugas dengan efisien, manajemen waktu, penugasan proyek, dan penilaian tugas yang efektif.</p>
                         </div>
                     </div>
                 </div>
@@ -103,8 +101,8 @@
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
                             <i class="fa fa-3x fa-globe text-primary mb-4"></i>
-                            <h5 class="mb-3">Online Classes</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <h5 class="mb-3">Manajemen Kelas</h5>
+                            <p>Manajemen kelas yang efektif untuk menciptakan lingkungan belajar yang inklusif dan produktif.</p>
                         </div>
                     </div>
                 </div>
@@ -112,8 +110,8 @@
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
                             <i class="fa fa-3x fa-home text-primary mb-4"></i>
-                            <h5 class="mb-3">Home Projects</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <h5 class="mb-3">Manajemen Ujian</h5>
+                            <p>Mengelola ujian dan penilaian. Mencakup pembuatan soal ujian, penerapan ujian daring, analisis hasil, dan memberikan umpan balik yang konstruktif.</p>
                         </div>
                     </div>
                 </div>
@@ -121,8 +119,8 @@
                     <div class="service-item text-center pt-3">
                         <div class="p-4">
                             <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
-                            <h5 class="mb-3">Book Library</h5>
-                            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                            <h5 class="mb-3">Penilaian</h5>
+                            <p>Penilaian yang efektif untuk mengukur kemajuan dan pencapaian siswa. Mencakup berbagai metode penilaian, termasuk ujian, proyek, penugasan, dan evaluasi keterampilan.</p>
                         </div>
                     </div>
                 </div>
@@ -143,30 +141,8 @@
                 </div>
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                     <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
-                    <h1 class="mb-4">Welcome to eLEARNING</h1>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                    <div class="row gy-2 gx-4 mb-4">
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Skilled Instructors</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>Online Classes</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>International Certificate</p>
-                        </div>
-                    </div>
-                    <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                    <h1 class="mb-4">Selamat Datang di eLEARNING</h1>
+                    <p class="mb-4">Selamat datang di eLEARNING, platform pembelajaran online terbaik untuk pengembangan diri dan pencapaian akademik Anda. Kami berkomitmen untuk memberikan pengalaman pembelajaran yang unik dan bermanfaat kepada semua peserta didik.</p>
                 </div>
             </div>
         </div>
