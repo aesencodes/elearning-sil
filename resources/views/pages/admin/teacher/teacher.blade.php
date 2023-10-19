36480982915471e081e629f6d840cb6a36dab0b1
TheChosenOne29 <gilbertwuaten@gmail.com>
2023-10-19 19:08:20 +0700

    fixed delete teachers

--- resources/views/pages/admin/teacher/teacher.blade.php
+++ resources/views/pages/admin/teacher/teacher.blade.php
@@ -14,31 +14,39 @@
                 <a href="{{ route('admin.teacher.create') }}" class="btn btn-primary">Add new Teacher</a>
             </div>
             <div class="card-body">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">No.</th>
-                            <th scope="col">NUPTK</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($teacher as $item)
+                <div class="table-responsive">
+                    <table class="table" style="overflow-x: auto">
+                        <thead>
                             <tr>
-                                <th scope="row" class="col-2">
-                                    {{ $item->seq_id }}
-                                </th>
-                                <td class="col-3">{{ $item->nuptk }}</td>
-                                <td class="col-4">{{ $item->name }}</td>
-                                <td class="col-2">
-                                    <a href="{{ route('admin.teacher.edit', $item->id) }}" class="btn btn-warning btn-sm mb-1">Edit</a>
-                                    <a href="{{ route('admin.teacher.destroy', $item->id) }}" class="btn btn-danger btn-sm mb-1">Delete</a>
-                                </td>
+                                <th scope="col">No.</th>
+                                <th scope="col">NUPTK</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Action</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($teacher as $item)
+                                <tr>
+                                    <th scope="row" class="col-2">
+                                        {{ $item->seq_id }}
+                                    </th>
+                                    <td class="col-3">{{ $item->nuptk }}</td>
+                                    <td class="col-4">{{ $item->name }}</td>
+                                    <td class="col-2">
+                                        <div class="btn-group" role="group">
+                                            <a href="{{ route('admin.teacher.edit', $item->id) }}" class="btn btn-warning btn-sm me-2">Edit</a>
+                                            <form action="{{ route('admin.teacher.destroy', $item->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     @endsection
